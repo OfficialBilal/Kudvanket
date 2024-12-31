44bf25b0ad6d3a0697f44ad53e4bc375a3f60d5e
OfficialBilal <chbilal.official@gmail.com>
2024-12-31 20:57:06 +0500

    Add project files.

---
+++ Kudvanket/Static-Instance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    class Circle
+    {
+        static float PI;
+        int _Radius;
+
+        static Circle()
+        {
+            Circle.PI = 3.141F;
+        }
+
+        public Circle(int Radius)
+        {
+            this._Radius = Radius;
+        }
+
+        public float CalculateArea()
+        {
+            return Circle.PI * this._Radius * this._Radius;
+        }
+    }
+
+
+    class Static_Instance
+    {
+        public static void Main(string[] args)
+        {
+            Circle C1 = new Circle(5);
+            float Area = C1.CalculateArea();
+
+            Console.WriteLine("Area of Cirlce is {0}", Area);
+        }
+    }
+}
