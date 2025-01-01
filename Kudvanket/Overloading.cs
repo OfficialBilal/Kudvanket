f21f736a7a70887d402bc087e2ba04b681c6f0b6
OfficialBilal <chbilal.official@gmail.com>
2025-01-01 20:58:41 +0500

    Overloading

---
+++ Kudvanket/Overloading.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    class Overloading
+    {   /*Overloading mean Multiple method with same name but method should be of different signature
+        (Signature include name of method, type of parameter and a kind of paramter)*/
+        public static void Main(string[] args)
+        {
+            Add(20, 20);
+        }
+
+        public static void Add(int F, int S)
+        {
+            Console.WriteLine("Sum is {0}", F+S);
+        }
+
+        public static void Add(int F, int S, int T)
+        {
+            Console.WriteLine("Sum is {0}", F + S + T);
+        }
+
+        public static void Add(int F, int S, int T, int E)
+        {
+            Console.WriteLine("Sum is {0}", F + S + T + E);
+        }
+
+    }
+}
