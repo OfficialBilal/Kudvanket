4cd6c782fd82bd71a137fa9aec58ba78000f0abe
OfficialBilal <chbilal.official@gmail.com>
2025-01-02 20:11:02 +0500

    Abstract Classes

---
+++ Kudvanket/AbstractClasses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    abstract class Customer()
+    {
+        public abstract void Print();
+    }
+    class AbstractClasses : Customer
+    { //Abstract Classes cannot be sealed
+        public override void Print()
+        {
+            Console.WriteLine("Abstarct Class");
+        }
+        public static void Main(string[] args)
+        {
+            Customer C1 = new AbstractClasses();
+            C1.Print();
+        }
+    }
+}
