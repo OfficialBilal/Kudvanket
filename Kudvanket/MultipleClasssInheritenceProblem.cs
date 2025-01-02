eb1ff9769c877d4bd639a7ea4d46292023482dbe
OfficialBilal <chbilal.official@gmail.com>
2025-01-02 20:37:49 +0500

    Multiple Classs Inheritence Problems

---
+++ Kudvanket/MultipleClasssInheritenceProblem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    class A
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("A Class");
+        }
+    }
+
+    class B : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("B Class");
+        }
+    }
+
+    class C : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("C Class");
+        }
+    }
+
+    class D: B,C 
+    {
+        //Diamond Problem
+    }
+    
+
+    class MultipleClasssInheritenceProblem
+    {
+        public static void Main(string[] args)
+        {
+            D d = new D();
+            d.Print();
+        }
+    }
+}
