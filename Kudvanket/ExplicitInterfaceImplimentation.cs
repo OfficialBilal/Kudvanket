961c24c50f5300d3ac4e532c1ec19eb5a259d87d
OfficialBilal <chbilal.official@gmail.com>
2025-01-02 19:41:03 +0500

    Explicit Interface Implimentation

---
+++ Kudvanket/ExplicitInterfaceImplimentation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    interface I1
+    {
+        void InterfaceMethod();
+    }
+
+    interface I2
+    {
+        void InterfaceMethod();
+    }
+    class ExplicitInterfaceImplimentation : I1, I2
+    {
+        void  I1.InterfaceMethod()
+        {
+            Console.WriteLine("I1 Interface Method");
+        }
+        void I2.InterfaceMethod()
+        {
+            Console.WriteLine("I2 Interface Method");
+        }
+        public static void Main(string[] args)
+        {
+            ExplicitInterfaceImplimentation C = new ExplicitInterfaceImplimentation();
+            ((I1)C).InterfaceMethod();
+            ((I2)C).InterfaceMethod();
+
+        }
+    }
+}
