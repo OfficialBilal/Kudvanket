7dfea5728a4acf142e21d8c8297cfcaea37408d6
OfficialBilal <chbilal.official@gmail.com>
2025-01-02 20:26:23 +0500

    Abstract and Interface

---
+++ Kudvanket/Abstract-Interface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    public abstract class Customers
+    {
+        int id;
+        public void Prints()
+        {
+            Console.WriteLine("Abstract Class");
+        }
+    }
+    public interface ICustomers
+    {
+        void Prints();
+    }
+
+    class Abstract_Interface
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
