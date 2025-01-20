fcddc73edc9f201484ffa61640f0b744fdfc3f1e
OfficialBilal <chbilal.official@gmail.com>
2025-01-20 20:19:45 +0500

    Delegates

---
+++ Kudvanket/Delegates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    public delegate void HelloDelagate(string message);
+
+    class Delegates
+    {
+        public static void Main(string[] args)
+        {
+            HelloDelagate del = new HelloDelagate(Hello);
+            del("Hello from Delegate");
+        }
+
+        public static void Hello(string strMessage) 
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+}
