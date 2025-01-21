884622e86361549ce4a0ff3001ef9b30a00d4760
OfficialBilal <chbilal.official@gmail.com>
2025-01-21 21:47:38 +0500

    Muslticast Delegates

---
+++ Kudvanket/Muslticast Delegates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    public delegate int SampleDelegate();
+    class Muslticast_Delegates
+    {
+        public static void Main(string[] args)
+        {
+            SampleDelegate del = new SampleDelegate(SampleMethodOne);
+            del += SampleMethodTwo;
+
+            int DelegateReturnValue = del();
+
+            Console.WriteLine(DelegateReturnValue);
+
+        }
+
+        public static int SampleMethodOne()
+        {
+            return 1;
+        }
+
+        public static int SampleMethodTwo()
+        {
+            return 2;
+        }
+
+        public static int SampleMethodThree()
+        {
+            return 3;
+        }
+    }
+}
