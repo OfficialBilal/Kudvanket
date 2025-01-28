f56d6a371b59481254a93e6d9bf4e1776b33f1a0
OfficialBilal <chbilal.official@gmail.com>
2025-01-28 20:38:19 +0500

    Exception Handling

---
+++ Kudvanket/ExceptionHandling.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    class ExceptionHandling
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"D:\Data.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("Plaese check is the file {0} exist", ex.FileName);
+            }
+            catch(Exception  ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                streamReader.Close();
+            }
+        }
+    }
+}
