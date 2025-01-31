be76b6f38c5ada3105165d3bcfcd0cc5218e2088
OfficialBilal <chbilal.official@gmail.com>
2025-01-31 18:18:32 +0500

    Custom Class

---
+++ Kudvanket/CustomClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    class CustomClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserAlreadyLoggedInException("User Already Logged In");
+            }
+            catch(UserAlreadyLoggedInException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    [Serializable]
+    class UserAlreadyLoggedInException : Exception 
+    {
+        public UserAlreadyLoggedInException() : base()
+        {
+
+        }
+        public UserAlreadyLoggedInException(string message) : base(message)
+        {
+
+        }
+        public UserAlreadyLoggedInException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+        public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
