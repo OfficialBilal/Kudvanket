810ce9d5389e03ab17909023a69ee51e5710198d
OfficialBilal <chbilal.official@gmail.com>
2025-03-06 17:34:41 +0500

    Attributes

---
+++ Kudvanket/52-Attributes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kudvanket
+{
+    public class _52_Attributes
+    {
+        public static void Main(string[] args)
+        {
+            
+            int num = Calculator.Add(new List<int> { 10, 20, 30 });
+
+            Console.WriteLine(num);
+        }
+    }
+
+    public class Calculator
+    {
+        [Obsolete("Use Add(List<int> Number) Method", true)]
+        public static int Add(int FirstNumber, int SecondNumber)
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public static int Add(List<int> Number)
+        {
+            int Sum = 0;
+
+            foreach (int number in Number)
+            {
+                Sum = Sum + number;
+            }
+
+            return Sum;
+        }
+    }
+}
